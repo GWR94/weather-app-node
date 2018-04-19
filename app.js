8db8a325a9caa01f56cffce98f79dd04e83ba861
jamesgower <james_gower@outlook.com>
2018-04-19 12:12:13 +0100

    key ignored and error handling completed

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const request = require('request');
 const yargs = require('yargs');
+const keys = require('./keys/dev');
 
 const argv = yargs
 	.options({
@@ -13,12 +14,18 @@ const argv = yargs
 	.help()
 	.alias('help', 'h')
 	.argv;
+	var encodedAddress = encodeURIComponent(argv.address);
 request({
-	url: 'https://maps.googleapis.com/maps/api/geocode/json?address=32%20ashden%20walk%20tonbridge&key=AIzaSyDtOLh7XJAtaRcygmcRARzm0mWHAQTtOW0',
+	url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${keys.APIKey}`,
 	json: true
 }, (error, response, body) => {
-	console.log(`Address: ${body.results[0].formatted_address}`);
-	console.log(`Latitude: ${body.results[0].geometry.location.lat}`)
-	console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
-	
+	if(error) {
+		console.log('Unable to connect to Google servers.');
+	} else if(body.status==='ZERO_RESULTS') {
+		console.log('Unable to retrieve information from that address.');
+	} else if(body.status==='OK') {
+		console.log(`Address: ${body.results[0].formatted_address}`);
+		console.log(`Latitude: ${body.results[0].geometry.location.lat}`)
+		console.log(`Longitude: ${body.results[0].geometry.location.lng}`)	
+	}
 });
